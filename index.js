4a8d63d13cda08da6a65b8b7367c82c8ca7a8d9e
NeeEoo <NeeEoo@users.noreply.github.com>
2020-10-10 21:59:28 +0200

    Added support for png images

--- index.js
+++ index.js
@@ -1,16 +1,34 @@
 const jpegJs = require('jpeg-js');
+const pngJs = require('pngjs');
 const Encoder = require('./lib/encoder.js');
+
+function compare(a, b) {
+  for (let i = 0; i < a.length; i++) {
+    if ((a[i] !== b[i])) {
+      return false;
+    }
+  }
+  return true;
+}
 
 module.exports = async (buffer) => {
   if (!(buffer instanceof Uint8Array)) {
     throw new Error('the input data should be a Buffer or Uint8Array');
   }
 
-  const { width, height, data } = jpegJs.decode(buffer);
+  let decodedBuffer;
+
+  if (compare(buffer.slice(0, 8), [0x89, 0x50, 0x4E, 0x47, 0xD, 0xA, 0x1A, 0xA])) { // PNG File Signature
+    decodedBuffer = pngJs.PNG.sync.read(buffer);
+  } else {
+    decodedBuffer = jpegJs.decode(buffer);
+  }
+
+  const { width, height, data } = decodedBuffer;
 
   const agbr = new Uint8Array(data.length);
 
-  for (let i = 0; i < data.length; i = i + 4) {
+  for (let i = 0; i < data.length; i += 4) {
     agbr[i + 0] = data[i + 3];
     agbr[i + 1] = data[i + 2];
     agbr[i + 2] = data[i + 1];
@@ -18,7 +36,6 @@ module.exports = async (buffer) => {
   }
 
   const encoder = new Encoder({ data: agbr, width, height });
-  encoder.encode;
 
   return encoder.data;
 };
